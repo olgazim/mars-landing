a5211554880ce8dc724e8a353f9aec503f659f91
Olga Zimina <olga.vic.zimina@gmail.com>
2023-03-17 23:45:59 -0700

    Add javadocs for ShipType

---
+++ src/main/java/ShipType.java
@@ -0,0 +1,17 @@
+/**
+ * Enum representing the different types of spaceships.
+ *
+ * <p>The available seat classes are:
+ * <ul>
+ *  <li>FALCON9 - representing a basic spaceship
+ *  <li>FALCON_HEAVY - representing a heavy-duty spaceship
+ *  <li>BFR - representing a high-tech spaceship
+ * </ul>
+ * @author Olga Zimina
+ * @version 1.0
+ */
+public enum ShipType {
+    FALCON9,
+    FALCON_HEAVY,
+    BFR
+}
