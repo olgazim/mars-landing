3a9bae5ff66548d5d8a6fdb5a09643faf556ed6f
Olga Zimina <olga.vic.zimina@gmail.com>
2023-04-05 18:44:36 -0700

    Add moveSubScene

--- src/main/java/com/example/marslanding/model/MarsLanderSubScene.java
+++ src/main/java/com/example/marslanding/model/MarsLanderSubScene.java
@@ -1,5 +1,6 @@
 package com.example.marslanding.model;
 
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.scene.SubScene;
 import javafx.scene.layout.AnchorPane;
@@ -7,15 +8,34 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class MarsLanderSubScene extends SubScene {
     private static final int HEIGHT = 300;
     private static final int WIDTH = 400;
+    private boolean isPresented = false;
 
     public MarsLanderSubScene() {
         super(new AnchorPane(), WIDTH, HEIGHT);
         AnchorPane rootPane = (AnchorPane) this.getRoot();
         BackgroundFill bgFill = new BackgroundFill(Color.web("#6C5B7B"), CornerRadii.EMPTY, Insets.EMPTY);
         rootPane.setBackground(new Background(bgFill));
+        setLayoutX(1024);
+        setLayoutY(180);
+    }
+
+    public void moveSubScene() {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setDuration(Duration.seconds(0.5));
+        transition.setNode(this);
+        if(!isPresented) {
+//            how to change position
+            transition.setToX(-550);
+            isPresented = true;
+        } else {
+            transition.setToX(0);
+            isPresented = false;
+        }
+        transition.play();
     }
 }
