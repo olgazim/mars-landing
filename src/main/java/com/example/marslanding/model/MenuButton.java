53e83d773457461023e9acdd98eccf04ca3858ea
Olga Zimina <olga.vic.zimina@gmail.com>
2023-04-09 14:18:41 -0700

    Remove unused imports

--- src/main/java/com/example/marslanding/model/MenuButton.java
+++ src/main/java/com/example/marslanding/model/MenuButton.java
@@ -2,10 +2,8 @@ package com.example.marslanding.model;
 
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 import java.io.FileInputStream;
