ace3c42858e5f9250f105594ef22c152874a3fba
Olga Zimina <olga.vic.zimina@gmail.com>
2023-04-09 15:01:56 -0700

    Add LandingZone

---
+++ src/main/java/com/example/marslanding/model/LandingZone.java
@@ -0,0 +1,66 @@
+package com.example.marslanding.model;
+
+import javafx.geometry.Bounds;
+import javafx.scene.image.ImageView;
+
+public class LandingZone {
+    private final ImageView imageView;
+    private final double maxXLimit;
+    private final double minXLimit;
+    private final double maxYLimit;
+    private final double minYLimit;
+    private static final double LANDING_LINE_COEFFICIENT = 0.6;
+    public LandingZone(
+            final String imagePath,
+            final double maxXLimit,
+            final double minXLimit,
+            final double maxYLimit,
+            final double minYLimit
+    ) {
+        this.imageView = new ImageView(imagePath);
+        this.maxXLimit = maxXLimit;
+        this.minXLimit = minXLimit;
+        this.maxYLimit = maxYLimit;
+        this.minYLimit = minYLimit;
+    }
+
+    private static double generateRandomDoubleInRange(final double min, final double max) {
+        return (Math.random() * (max - min)) + min;
+    }
+
+    private double getWidth() {
+        return imageView.getBoundsInLocal().getWidth();
+    }
+
+    private void setPosition(final double xCoordinate, final double yCoordinate) {
+        imageView.setX(x);
+        imageView.setY(y);
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setRandomPosition() {
+        final double xCoordinate = generateRandomDoubleInRange(minXLimit, maxXLimit - getWidth());
+        final double yCoordinate = generateRandomDoubleInRange(minYLimit, maxYLimit);
+
+        setPosition(xCoordinate, yCoordinate);
+    }
+
+    public void setScaleX(final double value) {
+        imageView.setScaleX(value);
+    }
+
+    public Bounds getBounds() {
+        return imageView.getBoundsInParent();
+    }
+
+    public double getBaseLine() {
+        final Bounds bounds = getBounds();
+        final double height = bounds.getHeight();
+        final double y = imageView.getLayoutY();
+
+        return maxYLimit + height * LANDING_LINE_COEFFICIENT;
+    }
+}
