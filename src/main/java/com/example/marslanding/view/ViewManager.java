11e28fc4c45f6ec563711270b5bb1d151848eae5
maz1995 <shuyiliu95@gmail.com>
2023-04-09 22:34:59 -0700

    Add Javadoc for ViewManager

--- src/main/java/com/example/marslanding/view/ViewManager.java
+++ src/main/java/com/example/marslanding/view/ViewManager.java
@@ -21,25 +21,45 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
-
+/**
+ This class represents the view manager for the Mars Lander game, which creates and manages
+ the user interface components of the game.
+ */
 public class ViewManager {
+    // width of the game screen
     private static final int WIDTH = 1024;
+    // height of the game screen
     private static final int HEIGHT = 648;
+    // the start x coordinate of menu button
     private static final int MENU_BUTTON_START_X = 100;
+    // the start y coordinate of menu button
     private static final int MENU_BUTTON_START_Y = 200;
+    // logo's height
     private static final int LOGO_HEIGHT = 120;
+    // logo's width
     private static final int LOGO_WIDTH = 100;
+    // logo's x coordinate
     private static final int LOGO_X_POSITION = 900;
+    // logo's y coordinate
     private static final int LOGO_Y_POSITION = 230;
+    // path to the font used in the game
     private final String FONT_PATH = "src/main/java/com/example/marslanding/model/resources/kenvector_future.ttf";
-
+    // an AnchorPane object
     private AnchorPane anchorPane;
+    // a Scene object
     private Scene scene;
+    // a Stage object
     private Stage stage;
+    // a list of MenuButton objects
     private List<MenuButton> menuButtonList;
+    // a MarsLanderSubScene object scoreSubScene
     private MarsLanderSubScene scoreSubScene;
 
+    /**
+     * Constructs a new ViewManager object.
+     */
     public ViewManager() {
         anchorPane = new AnchorPane();
         scene = new Scene(anchorPane, WIDTH, HEIGHT);
@@ -52,10 +72,16 @@ public class ViewManager {
         createScoreSubScenes();
     }
 
+    /**
+     * Returns the stage of the view manager.
+     * @return The stage of the view manager.
+     */
     public Stage getStage() {
         return stage;
     }
 
+    // Creates the logo for the game, with an astronaut image and a drop shadow effect. When
+    // the mouse enters or exits the image, the drop shadow effect changes.
     private void createLogo() {
         ImageView astronaut = new ImageView("astronaut.png");
         astronaut.setFitHeight(LOGO_HEIGHT);
@@ -78,6 +104,7 @@ public class ViewManager {
         anchorPane.getChildren().add(astronaut);
     }
 
+    // Creates the "Play" button for the game, which starts a new game when clicked.
     private void createPlayButton() {
         MenuButton playBtn = new MenuButton("Play");
         addMenuButtons(playBtn);
@@ -90,7 +117,7 @@ public class ViewManager {
             }
         });
     }
-
+    // Creates the "Exit" button for the game, which exits the application when clicked.
     private void createExitButton() {
         MenuButton exitBtn = new MenuButton("Exit");
         addMenuButtons(exitBtn);
@@ -101,6 +128,8 @@ public class ViewManager {
             }
         });
     }
+    // Creates the "Score" button for the game, which shows the player's score when clicked.
+    // The score is read from a file named "score.txt" and displayed on the screen.
     private void createScoreButton() {
         MenuButton scoreBtn = new MenuButton("Score");
         addMenuButtons(scoreBtn);
@@ -126,12 +155,15 @@ public class ViewManager {
         });
     }
 
+    // Creates all menu buttons for the game.
     private void createMenuButtons() {
         createPlayButton();
         createScoreButton();
         createExitButton();
     }
 
+    // Adds a menu button to the list of buttons and to the game scene.
+    // @param button the button to add
     private void addMenuButtons(final MenuButton button) {
         button.setLayoutX(MENU_BUTTON_START_X);
         button.setLayoutY(MENU_BUTTON_START_Y + menuButtonList.size() * 100);
@@ -140,7 +172,8 @@ public class ViewManager {
 
     }
 
-//TODO: fix image path so we could put image to resources
+    // Creates the game's background image and sets it as the game's background.
+    // The image is read from a file named "galaxy_background.png".
     private void createBackground() {
         // get image file
         Image background = new Image("galaxy_background.png", 1024, 648, false, true);
@@ -151,8 +184,50 @@ public class ViewManager {
         anchorPane.setBackground(new Background(backgroundImage));
     }
 
+    // Creates the score sub-scene, which is used to display the player's score.
     private void createScoreSubScenes() {
         scoreSubScene = new MarsLanderSubScene();
         anchorPane.getChildren().add(scoreSubScene);
+    }
+
+    /**
+     * Returns a string representation of the ViewManager object.
+     *
+     * @return A string representation of the ViewManager object.
+     */
+    @Override
+    public String toString() {
+        return "ViewManager{" +
+                "FONT_PATH='" + FONT_PATH + '\'' +
+                ", anchorPane=" + anchorPane +
+                ", scene=" + scene +
+                ", stage=" + stage +
+                ", menuButtonList=" + menuButtonList +
+                ", scoreSubScene=" + scoreSubScene +
+                '}';
+    }
+
+    /**
+     * Compares this ViewManager object to the given object.
+     *
+     * @param o The object that is compared to this ViewManager object
+     * @return True - if the given object is of type ViewManager with the same state as this object,
+     * false - otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ViewManager that = (ViewManager) o;
+        return Objects.equals(FONT_PATH, that.FONT_PATH) && Objects.equals(anchorPane, that.anchorPane) && Objects.equals(scene, that.scene) && Objects.equals(stage, that.stage) && Objects.equals(menuButtonList, that.menuButtonList) && Objects.equals(scoreSubScene, that.scoreSubScene);
+    }
+
+    /**
+     * Returns a hash code for this ViewManager object.
+     * @return A hash code value for this ViewManager object
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(FONT_PATH, anchorPane, scene, stage, menuButtonList, scoreSubScene);
     }
 }
