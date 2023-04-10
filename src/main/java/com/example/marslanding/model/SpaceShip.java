bd65036be6dc6aab030b9432b76c4cd7464c2c84
Olga Zimina <olga.vic.zimina@gmail.com>
2023-04-09 18:28:22 -0700

    Remove unused code

--- src/main/java/com/example/marslanding/model/SpaceShip.java
+++ src/main/java/com/example/marslanding/model/SpaceShip.java
@@ -1,6 +1,6 @@
 package com.example.marslanding.model;
 
-import com.example.marslanding.ShipType;
+import javafx.scene.image.ImageView;
 
 /**
  * Represents a spaceship.
@@ -9,115 +9,94 @@ import com.example.marslanding.ShipType;
  * @version 1.0
  */
 public class SpaceShip {
-    // The mass of the Falcon 9 spaceship in kilograms
-    public static final int FALCON9_MASS = 152;
-    // The mass of the Falcon Heavy spaceship in kilograms
-    public static final int FALCON_HEAVY_MASS = 170;
-    // The mass of the Big Falcon Rocket (BFR) spaceship in kilograms
-    public static final int BFR_MASS = 200;
-    // The amount of fuel in the tank of the Falcon 9 spaceship
-    public static final double FALCON9_FUEL_AMOUNT = 70;
-    // The amount of fuel in the tank of the Falcon Heavy spaceship
-    public static final double FALCON_FUEL_AMOUNT = 90;
-    // The amount of fuel in the tank of the BFR spaceship
-    public static final double BFR_FUEL_AMOUNT = 130;
-    // The mass of the spaceship in kilograms
-    private int mass;
-    // The amount of fuel in the spaceship's tank
-    private double fuelAmount;
-    // The type of the spaceship
-    private ShipType type;
-    // The current burn rate of the spaceship's engines
-    private double currentBurnRate;
+    public static final String FALCON9_IMAGE = "small_space_ship.png";
+    public static final String BFR_IMAGE = "small_space_ship.png";
+    public static final String FALCON_HEAVY_IMAGE = "small_space_ship.png";
+    public static final int FALCON9_SIZE = 50;
+    private final double limitX, limitY;
     // The current coordinates of the spaceship
-    private double[] currentCoordinates;
-    // The current velocity of the spaceship
-    private double velocity;
-    // The number of coins collected by the spaceship
-    private int coins;
-    // The maximum velocity at which the spaceship can land safely
-    private double maxLandingVelocity;
-    // The width of the spaceship in meters
-    private int width;
+    private double[] currentCoordinates = new double[2];
+    private final ShipType type;
+    private final ImageView shipImage;
+    private final double shipImgHeight;
+    private final double shipImgWidth;
 
     /**
-     * Constructs a com.example.marslanding.model.SpaceShip object based on specified type.
-     * @param type the type of the spaceship, of type com.example.marslanding.ShipType
+     * Constructs a SpaceShip object based on specified type.
+     * @param type the type of the spaceship, of type ShipType
      */
-    public SpaceShip(final ShipType type) {
+    public SpaceShip(final ShipType type,
+                     final double limitX,
+                     final double limitY) {
+        this.type = type;
+        this.limitX = limitX;
+        this.limitY = limitY;
+        currentCoordinates[0] = limitX / 2;
+        currentCoordinates[1] = limitY / 2;
         switch (type) {
             case FALCON_HEAVY -> {
-                mass = FALCON_HEAVY_MASS;
-                fuelAmount = FALCON_FUEL_AMOUNT;
+                shipImage = new ImageView(FALCON9_IMAGE);
+                shipImgHeight = FALCON9_SIZE;
+                shipImgWidth = FALCON9_SIZE;
+                break;
             }
             case BFR -> {
-                mass = BFR_MASS;
-                fuelAmount = BFR_FUEL_AMOUNT;
+                shipImage = new ImageView(FALCON9_IMAGE);
+                shipImgHeight = FALCON9_SIZE;
+                shipImgWidth = FALCON9_SIZE;
+                break;
             }
             default -> {
-                mass = FALCON9_MASS;
-                fuelAmount = FALCON9_FUEL_AMOUNT;
+                shipImage = new ImageView(FALCON9_IMAGE);
+                shipImgHeight = FALCON9_SIZE;
+                shipImgWidth = FALCON9_SIZE;
             }
+        }
+        setupImageView(shipImgHeight, shipImgWidth, currentCoordinates);
+    }
+
+    public ShipType getType() {
+        return type;
+    }
+
+    public ImageView getShipImage() {
+        return shipImage;
+    }
+
+    public double getShipImgHeight() {
+        return shipImgHeight;
+    }
+
+    public double getShipImgWidth() {
+        return shipImgWidth;
+    }
+    private void setupImageView(final double height, final double width, final double[] currentCoordinates) {
+        shipImage.setFitWidth(width);
+        shipImage.setFitHeight(height);
+        shipImage.setLayoutY(currentCoordinates[1]);
+        shipImage.setLayoutX(currentCoordinates[0]);
+    }
+
+    private void moveImgTo(final double[] coordinates) {
+        shipImage.relocate(coordinates[0], coordinates[1]);
+    }
+
+    public void moveShipBy(final double deltaX, final double deltaY) {
+        if (deltaX == 0 && deltaY == 0) {
+            return;
+        }
+
+        double newX = shipImage.getLayoutX() + deltaX;
+        double newY = shipImage.getLayoutY() + deltaY;
+        boolean isXValid = newX >= 0 && getShipImgWidth() <= limitX;
+        boolean isYValid = newY >= 0 && getShipImgHeight() <= limitY;
+        if (isXValid && isYValid) {
+            double[] newCoordinates = {newX, newY};
+            moveImgTo(newCoordinates);
         }
     }
 
-    /**
-     * Gets the fuel amount of the spaceship.
-     * @return the fuel amount of the spaceship as double
-     */
-    public double getFuelAmount() {
-        return fuelAmount;
+    public double findBottomLine() {
+        return shipImage.getLayoutY() + shipImgHeight;
     }
-
-    /**
-     * Gets the current coordinates of the spaceship.
-     * @return the current coordinates of the spaceship as array of doubles,
-     * where first element represents x-coordinate and second element represents y-coordinate
-     */
-    public double[] getCurrentCoordinates() {
-        return currentCoordinates;
-    }
-
-    /**
-     * Gets the number of coins collected by the spaceship.
-     * @return the number of coins collected by the spaceship as int
-     */
-    public int getCoins() {
-        return coins;
-    }
-
-    /**
-     * Sets the fuel amount of the spaceship to the specified value.
-     * @param fuelAmount the new fuel amount of the spaceship of type double
-     */
-    public void setFuelAmount(double fuelAmount) {
-        this.fuelAmount = fuelAmount;
-    }
-
-    /**
-     * Sets the current coordinates of the spaceship.
-     * @param currentCoordinates the new current coordinates of the spaceship as array of doubles,
-     * where first element represents x-coordinate and second element represents y-coordinate
-     */
-    public void setCurrentCoordinates(final double[] currentCoordinates) {
-        this.currentCoordinates = currentCoordinates;
-    }
-
-    /**
-     * Sets the number of coins collected by the spaceship.
-     * @param coins the new number of coins collected by the spaceship of type int
-     */
-    public void setCoins(final int coins) {
-        this.coins = coins;
-    }
-
-    /**
-     * Calculates the velocity of the spaceship based on the specified gravitational force of the planet.
-     * @param gravitation the gravitational force of the planet of type double
-     */
-    public void calculateVelocity(final double gravitation) {
-        velocity = Math.sqrt((mass + fuelAmount) * gravitation);
-    }
-
-    //TODO: toString, equals, hashCode
 }
