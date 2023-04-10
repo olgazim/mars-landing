c9c02e2d7a3550e0a170465578cb0cb3c4da6e67
Olga Zimina <olga.vic.zimina@gmail.com>
2023-04-09 22:11:10 -0700

    Add javadoc

--- src/main/java/com/example/marslanding/model/MenuButton.java
+++ src/main/java/com/example/marslanding/model/MenuButton.java
@@ -1,79 +1,91 @@
 package com.example.marslanding.model;
 
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-
+/**
+ * Represents a custom MenuButton class.
+ * A custom Popup class that represents a button with custom styles and behaviors for a menu.
+ * @author Olga Zimina
+ * @version 1
+ */
 public class MenuButton extends Button {
-    private final String FONT_PATH = "src/main/java/com/example/marslanding/model/resources/kenvector_future.ttf";
-    private final String BUTTON_PRESSED_STYLE = "-fx-background-color: #B7DEF1;";
-    private final String BUTTON_STYLE = "-fx-background-color: #E2F3EC; -fx-text-fill: #102F53";
+    // The path of the font file to be used for the label text
+    private static final String FONT_PATH = "src/main/java/com/example/marslanding/model/"
+            + "resources/kenvector_future.ttf";
+    // The default font to be used for the label text
+    private static final String DEFAULT_FONT = "Verdana";
+    // The style for the button when it is pressed
+    private static final String BUTTON_PRESSED_STYLE = "-fx-background-color: #B7DEF1;";
+    // The style for the button when it is not pressed
+    private static final String BUTTON_STYLE = "-fx-background-color: #E2F3EC; -fx-text-fill: #102F53";
+    // The height of the button
+    private static  final int BUTTON_HEIGHT = 60;
+    // The preferred height of the button
+    private static  final int BUTTON_PREF_HEIGHT = 55;
+    // The width of the button
+    private static  final int BUTTON_WIDTH = 300;
+    // The font size for the button text
+    private static final int FONT_SIZE = 25;
 
-    public MenuButton (String label) {
+    /**
+     * Constructs a new MenuButton with the given label text.
+     * @param label the text to be displayed on the button
+     */
+    public MenuButton(final String label) {
         setText(label);
         setButtonFont();
-        setPrefWidth(300); // should be equal to image width
-        setPrefHeight(55); // should be equal to image height
+        setPrefWidth(BUTTON_WIDTH);
+        setPrefHeight(BUTTON_PREF_HEIGHT);
         setStyle(BUTTON_STYLE);
-        initialiseButtonListeners();
+        initializeButtonListeners();
     }
-
+    /*
+    Sets the font of the button text to the font loaded from the FONT_PATH file.
+    If the file is not found, the default font is used instead.
+     */
     private void setButtonFont() {
         try {
-            setFont(Font.loadFont(new FileInputStream(FONT_PATH), 25));
+            setFont(Font.loadFont(new FileInputStream(FONT_PATH), FONT_SIZE));
         } catch (FileNotFoundException ex) {
-            setFont(Font.font("Verdana", 25));
+            setFont(Font.font(DEFAULT_FONT, FONT_SIZE));
         }
     }
-
+    /*
+    Sets the CSS style of the button to the pressed style and adjusts the height of the button.
+     */
     private void setButtonPressedStyle() {
         setStyle(BUTTON_PRESSED_STYLE);
-        setPrefHeight(60);
+        setPrefHeight(BUTTON_HEIGHT);
         setLayoutY(getLayoutY());
     }
-
+    /*
+    Sets the CSS style of the button to the default style and adjusts the height of the button.
+     */
     private void setButtonReleasedStyle() {
         setStyle(BUTTON_STYLE);
-        setPrefHeight(60);
+        setPrefHeight(BUTTON_HEIGHT);
         setLayoutY(getLayoutY());
     }
-
-    private void initialiseButtonListeners() {
-        setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getButton().equals(MouseButton.PRIMARY)) {
-                    setButtonPressedStyle();
-                }
+    /*
+    Initializes the button listeners for mouse presses, releases, enters, and exits.
+     */
+    private void initializeButtonListeners() {
+        setOnMousePressed(event -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                setButtonPressedStyle();
             }
         });
 
-        setOnMouseReleased(new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getButton().equals(MouseButton.PRIMARY)) {
-                    setButtonReleasedStyle();
-                }
-            }
-
-        });
-        setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                setStyle(BUTTON_PRESSED_STYLE);
+        setOnMouseReleased(event -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                setButtonReleasedStyle();
             }
         });
-        setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                setStyle(BUTTON_STYLE);
-            }
-        });
+        setOnMouseEntered(event -> setStyle(BUTTON_PRESSED_STYLE));
+        setOnMouseExited(event -> setStyle(BUTTON_STYLE));
     }
 }
